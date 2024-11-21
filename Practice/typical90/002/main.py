06f51d630e8c89134e817752ced9d15863ada428
firLambda <koki19991612@gmail.com>
2024-11-21 15:02:56 +0900

    solve problem b of typical 90

--- Practice/typical90/002/main.py
+++ Practice/typical90/002/main.py
@@ -1,5 +1,43 @@
 def main():
-    pass
+    # 0:(, 1:)
+    N = int(input())
+
+    ansList = []
+
+    if N % 2 == 0:
+        for mask in range(1 << N):
+            if not mask.bit_count() == N / 2:
+                # print(bin(mask), mask.bit_count())
+                continue
+            else:
+                uncouples = 0
+                ans = ""
+                # print(bin(mask))
+                for digitIndex in range(N):
+
+                    bitNum = 1 & (mask >> digitIndex)
+                    # print(bitNum)
+
+                    if bitNum == 0:
+                        uncouples += 1
+                        ans += "("
+                    elif bitNum == 1:
+                        uncouples -= 1
+                        ans += ")"
+                    if uncouples < 0:
+                        break
+
+                if uncouples == 0:
+                    ansList.append(ans)
+
+        if ansList:
+            ansList = sorted(ansList)
+
+            for ans in ansList:
+                print(ans)
+        # else:
+        #     print("")
+
 
 if __name__ == '__main__':
     main()
---
+++ arc187/a/main.py
@@ -0,0 +1,7 @@
+def main():
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    
+
+if __name__ == '__main__':
+    main()
